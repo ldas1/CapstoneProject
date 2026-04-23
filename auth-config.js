4c3eb2d1f499d2f4763d9af456520fdf85efd907
Luit Das <157326963+ldas1@users.noreply.github.com>
2026-04-22 19:16:05 -0500

    Add files via upload

---
+++ auth-config.js
@@ -0,0 +1,8 @@
+window.AUTH_CONFIG = {
+  cognitoDomain: "https://us-east-1l91cmq2hb.auth.us-east-1.amazoncognito.com",
+  clientId: "64an5cpadtqhe7kgf0ivgmc787",
+  redirectUri: "https://main.d3izqoj4p7aa12.amplifyapp.com/staff-orders.html",
+  logoutUri: "https://main.d3izqoj4p7aa12.amplifyapp.com/staff_login.html",
+  ownerGroup: "cheatDay_owner",
+  employeeGroup: "cheatDay_employees"
+};
